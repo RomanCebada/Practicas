36d2bf91189b13028f2a6bfa6d279ae09f1f0176
5j <5j@iMac-de-Hector.local>
2019-09-11 08:12:45 -0500

    first commit

---
+++ ej.java
@@ -0,0 +1,125 @@
+package jorge.android.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    private EditText editTextPhone;
+    private EditText editTextWeb;
+    private ImageButton imgBtnPhone;
+    private ImageButton imgBtnWeb;
+    private ImageButton imgBtnCamera;
+
+    private final int PHONE_CALL_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        editTextPhone = (EditText) findViewById(R.id.editTextPhone);
+        editTextWeb = (EditText) findViewById(R.id.editTextWeb);
+        imgBtnPhone = (ImageButton) findViewById(R.id.imageButtonPhone);
+        imgBtnWeb = (ImageButton) findViewById(R.id.imageButtonWeb);
+        imgBtnCamera = (ImageButton) findViewById(R.id.imageButtonCamera);
+
+        imgBtnPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phoneNumber = editTextPhone.getText().toString();
+                if (phoneNumber != null && !phoneNumber.isEmpty()) {
+                    //Comprueba version de android
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        // Comprobar los permisos
+                        Intent i = new Intent(Intent.ACTION_CALL, Uri.parse("Tel:"+phoneNumber));
+                        if (ActivityCompat.checkSelfPermission(ThirdActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) return;
+                            startActivity(i);
+                        } else {
+                            // Si no acepto o es la primera vez
+                            if(!shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)){
+                                // Si no se ha preguntado
+                                requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PHONE_CALL_CODE);
+                            }else{
+                                // Denegado
+                                Toast.makeText(ThirdActivity.this, "Open the door!", Toast.LENGTH_SHORT).show();
+                                Intent i = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                i.addCategory(Intent.CATEGORY_DEFAULT);
+                                i.setData(Uri.parse("package:" + getPackageName()));
+                                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                                i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                                startActivity(i);
+                            }
+                        }
+
+                    } else {
+                        //OlderVersions(phoneNumber);
+                    }
+                } else {
+                    Toast.makeText(ThirdActivity.this, "Insert a phone number", Toast.LENGTH_SHORT).show();
+                }
+              }
+        }
+
+
+            /*private void OlderVersions(String phoneNumber) {
+                Intent intentCall = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
+                if (CheckPermission(Manifest.permission.CALL_PHONE)) {
+                    if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    startActivity(intentCall);
+                } else {
+                    Toast.makeText(ThirdActivity.this, "I don't even know who you are", Toast.LENGTH_LONG).show();
+                }
+            }*/
+
+    });
+}
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PHONE_CALL_CODE:
+                String permission = permissions[0];
+                int result = grantResults[0];
+
+                if (permission.equals(Manifest.permission.CALL_PHONE)) {
+                    if (result == PackageManager.PERMISSION_GRANTED) {
+                        String phoneNumber = editTextPhone.getText().toString();
+                        Intent intentCall = new Intent(Intent.ACTION_CALL, Uri.parse("Tel:" + phoneNumber));
+                        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                            return;
+                        }
+                        startActivity(intentCall);
+                    } else {
+                        Toast.makeText(ThirdActivity.this, "I don't even know who you are", Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
+    }
+
+    private boolean CheckPermission(String permission){
+        int result = this.checkCallingOrSelfPermission(permission);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+}
